6195176bdefa6cc385b6983bc537d92df3dee8b5
pjha7 <prashant_jha1@intuit.com>
2024-02-29 23:50:06 +0530

    Pagination in VanillaJS

---
+++ index.js
@@ -0,0 +1,28 @@
+const mainDiv = document.getElementById('mainDiv');
+const pageDiv = document.getElementById('pageDiv');
+const pages = [];
+let currentPage = 0;
+let posts = []
+
+fetch('https://jsonplaceholder.typicode.com/posts')
+.then(response => response.json())
+.then(result => {
+    for(let index=0;index<result.length/10;index++){
+        pages.push(index)
+    }
+    posts = result
+    renderPosts(posts.slice(currentPage*10, (currentPage+1)*10));
+    renderPagination()
+})
+
+function renderPosts(posts){
+    mainDiv.innerHTML=posts.map(({title})=>`<p>${title}</p>`).join('');
+}
+
+function renderPagination(){
+    pageDiv.innerHTML+=pages.map((value, index)=>`<span onClick='updatePage(${index})' style="height: 30px; width: 30px; background-color: aqua; justify-content: center; align-items: center; margin-right: 5px;" >${value}</span>`).join('')
+}
+
+function updatePage(index){
+    renderPosts(posts.slice(index*10, (index+1)*10));
+}
